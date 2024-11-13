1b4ea96ea0ebd2d0ad464507b1d7a3651f120ba5
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-11-13 09:47:12 +0200

    Mkr1: practise part

---
+++ Mkr1/Mkr1.Tests/UnitTests.cs
@@ -0,0 +1,130 @@
+using Mkr1.App;
+using System.IO;
+
+namespace Mkr1.Test;
+
+public class Tests
+{
+    [Test]
+    [TestCase("agcta")]
+    [TestCase("aGcTa")]
+    [TestCase("AGCTA")]
+    public void IsDnaSequence_IsDnaSequence(string sequence)
+    {
+        Assert.That(
+            DnaSubsequenceChecker.IsDnaSequence(sequence),
+            Is.True,
+            $"'{sequence}' should be DNA sequence"
+        );
+    }
+
+    [Test]
+    [TestCase("agctfa")]
+    [TestCase("aGcTsa")]
+    [TestCase("AGCTAV")]
+    [TestCase("")]
+    public void IsDnaSequence_IsNotDnaSequence(string sequence)
+    {
+        Assert.That(
+            DnaSubsequenceChecker.IsDnaSequence(sequence),
+            Is.False,
+            $"'{sequence}' should not be DNA sequence"
+        );
+    }
+
+    [Test]
+    [TestCase("agcta", "gta")]
+    [TestCase("agCta", "gTA")]
+    [TestCase("AGGGGGC", "GGC")]
+    public void IsDnaSubsequence_IsDnaSubsequence(string sequence, string subsequence)
+    {
+        Assert.That(
+            DnaSubsequenceChecker.IsDnaSubsequence(sequence, subsequence),
+            Is.True,
+            $"'{subsequence}' should be subsequence of '{sequence}'"
+        );
+    }
+
+    [Test]
+    [TestCase("gTA", "agcta")]
+    [TestCase("agCtba", "gTA")]
+    [TestCase("AGGGGGC", "")]
+    [TestCase("AGGGGGC", "GAT")]
+    public void IsDnaSubsequence_IsNotDnaSubsequence(string sequence, string subsequence)
+    {
+        Assert.That(
+            DnaSubsequenceChecker.IsDnaSubsequence(sequence, subsequence),
+            Is.False,
+            $"'{subsequence}' should not be subsequence of '{sequence}'"
+        );
+    }
+
+    [Test]
+    [TestCase("agcta", "gta")]
+    [TestCase("agCta", "gTA")]
+    [TestCase("AGGGGGC", "GGC")]
+    public void Main_OutputFileMustContainYes(string sequence, string subsequence)
+    {
+        DirectoryInfo temporaryData = Directory.CreateDirectory("temp");
+
+        string inputFile = Path.Combine(".", "temp", "INPUT.TXT");
+        string outputFile = Path.Combine(".", "temp", "OUTPUT.TXT");
+
+        using (StreamWriter input = new StreamWriter(inputFile))
+        {
+            input.WriteLine(subsequence);
+            input.WriteLine(sequence);
+        }
+
+        Program.WriteAnswer(inputFile, outputFile);
+
+        using (StreamReader output = new StreamReader(outputFile))
+        {
+            string? answer = output.ReadLine();
+
+            Assert.That(
+                answer == "YES",
+                Is.True,
+                $"a combination of subsequence '{subsequence}' and " +
+                    $"sequence '{sequence}' must give 'YES'"
+            );
+        }
+
+        temporaryData.Delete(true); // true => recursive delete
+    }
+
+    [Test]
+    [TestCase("gTA", "agcta")]
+    [TestCase("agCtba", "gTA")]
+    [TestCase("AGGGGGC", "")]
+    [TestCase("AGGGGGC", "GAT")]
+    public void Main_OutputFileMustContainNo(string sequence, string subsequence)
+    {
+        DirectoryInfo temporaryData = Directory.CreateDirectory("temp");
+
+        string inputFile = Path.Combine(".", "temp", "INPUT.TXT");
+        string outputFile = Path.Combine(".", "temp", "OUTPUT.TXT");
+
+        using (StreamWriter input = new StreamWriter(inputFile))
+        {
+            input.WriteLine(subsequence);
+            input.WriteLine(sequence);
+        }
+
+        Program.WriteAnswer(inputFile, outputFile);
+
+        using (StreamReader output = new StreamReader(outputFile))
+        {
+            string? answer = output.ReadLine();
+
+            Assert.That(
+                answer == "NO",
+                Is.True,
+                $"a combination of subsequence '{subsequence}' and " +
+                    $"sequence '{sequence}' must give 'NO'"
+            );
+        }
+
+        temporaryData.Delete(true); // true => recursive delete
+    }
+}
